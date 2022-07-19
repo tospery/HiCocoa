2920784c970a4601cbc32087c09949f11335fc5a
yangjianxiang <yangjianxiang@xunyou.com>
2022-07-19 18:02:53 +0800

    frame2

---
+++ HiCocoa/Frame/Other/Runtime.swift
@@ -0,0 +1,28 @@
+//
+//  Runtime.swift
+//  HiCocoa
+//
+//  Created by liaoya on 2022/7/19.
+//
+
+import UIKit
+
+public protocol RuntimeCompatible {
+    func myWork()
+}
+
+final public class Runtime {
+
+    public static var shared = Runtime()
+    
+    public init() {
+    }
+    
+    public func work() {
+        ExchangeImplementations(UIViewController.self, #selector(UIViewController.present(_:animated:completion:)), #selector(UIViewController.swf_present(_:animated:completion:)))
+        if let compatible = self as? RuntimeCompatible {
+            compatible.myWork()
+        }
+    }
+    
+}
